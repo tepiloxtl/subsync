98c6c8dc9e092d7a6a509308f305b924a86f0c5c
Tepiloxtl <tepiloxtl@tepiloxtl.net>
2025-02-25 11:41:09 +0100

    more switches, more playlists, more better

--- subsync.py
+++ subsync.py
@@ -1,15 +1,27 @@
-import requests, pprint, json, argparse, re
+import requests, pprint, json, argparse, re, os
 from urllib.parse import urljoin
+from pathlib import Path
 
 with open("config.json", "r") as f:
     config = json.load(f)
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument("--artist", type=str)
-argparser.add_argument("--album", type=str)
-argparser.add_argument("--albumid", type=str)
-argparser.add_argument("--artists", default=False, action="store_true")
-argparser.add_argument("--albums", default=False, action="store_true")
+queries = argparser.add_argument_group("Queries")
+queries.add_argument("--artist", type=str)
+queries.add_argument("--album", type=str)
+queries.add_argument("--playlist", type=str)
+queries.add_argument("--albumid", type=str)
+queries.add_argument("--playlistid", type=str)
+listings = argparser.add_argument_group("List options")
+listings.add_argument("--list-artists", default=False, action="store_true", help="List all artists on the server")
+listings.add_argument("--list-albums", default=False, action="store_true", help="List artists albums or all albums on the server")
+listings.add_argument("--list-playlists", default=False, action="store_true", help="List all playlists on the server")
+_download_options = argparser.add_argument_group("Download options")
+download_options = _download_options.add_mutually_exclusive_group(required=False)
+download_options.add_argument("--download", "-D", action="store_true", help="Directly download files")
+download_options.add_argument("--transcode", "-T", action="store_true", help="Transcode and download files")
+download_options.add_argument("--m3u-only", action="store_true", help="Only write m3u files")
+argparser.add_argument("--dir", "-d", type=str)
 args = argparser.parse_args()
 
 artists = {}
@@ -92,6 +104,26 @@ def search_dict(d, search_key):
             except ValueError:
                 print("Invalid input. Please enter a number.")
 
+def download(list):
+    if "album" in list["subsonic-response"]:
+        listname = list["subsonic-response"]["album"]["artist"] + " - " + list["subsonic-response"]["album"]["name"]
+        ulist = list["subsonic-response"]["album"]["song"]
+    elif "playlist" in list["subsonic-response"]:
+        listname = list["subsonic-response"]["playlist"]["name"]
+        ulist = list["subsonic-response"]["playlist"]["entry"]
+    if args.download:
+        os.makedirs(Path("albums", listname))
+        for item in ulist:
+            filename, file = connection.get_binary("download", {"id": item["id"]})
+            with open(Path("albums", listname, filename), "wb") as f:
+                f.write(file)
+                f.close
+            print(filename)
+    elif args.transcode:
+        pass
+    elif args.m3u_only:
+        pass
+
 connection = subsonic_server(config["server"])
 connection.test_connection()
 
@@ -103,8 +135,12 @@ for item in request["subsonic-response"]["artists"]["index"]:
         artists[item2["name"]] = item2["id"]
 
 # Print list of artists
-if args.artists == True:
+if args.list_artists:
     pprint.pprint(artists)
+
+if args.list_playlists:
+    pprint.pprint(connection.get_json("getPlaylists"))
+    
 
 # Print out albums by artist
 if args.artist and not args.album:
@@ -117,16 +153,24 @@ if args.artist and args.album:
     artist = search_dict(artists, args.artist)
     albums = get_albums_byartist(artist)
     albumid = search_dict(albums, args.album)
-    tracks = connection.get_json("getAlbum", {"id": albumid})
+    album = connection.get_json("getAlbum", {"id": albumid})
     #filename, file = connection.get_binary()
-    pprint.pprint(tracks)
-    for item in tracks["subsonic-response"]["album"]["song"]:
-        filename, file = connection.get_binary("download", {"id": item["id"]})
-        with open(filename, "wb") as f:
-            f.write(file)
-            f.close
-        print(filename)
+    # with open("album.txt", "w", encoding="utf-8") as f:
+    #     pprint.pprint(album, f)
+    download(album)
 
 #idk what to do if only album is given
 if args.album and not args.artist:
     print("asdf3")
+
+if args.playlist:
+    playlists = connection.get_json("getPlaylists")
+    #TODO this is probably some convoluted list comprehension
+    playlists2 = {}
+    for item in playlists["subsonic-response"]["playlists"]["playlist"]:
+        playlists2[item["name"]] = item["id"]
+    playlistid = search_dict(playlists2, args.playlist)
+    playlist = connection.get_json("getPlaylist", {"id": playlistid})
+    # with open("playlist.txt", "w", encoding="utf-8") as f:
+    #     pprint.pprint(playlist, f)
+    download(playlist)
